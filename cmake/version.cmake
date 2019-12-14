cff645aea5d5c70d6a173c6a7b54f87a6f1234e3
Alessandro Gario <alessandro.gario@gmail.com>
2019-12-14 03:35:01 +0100

    CI: Automatically draft releases for v* tags

---
+++ cmake/version.cmake
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019-present, Trail of Bits, Inc.
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+#
+
+cmake_minimum_required(VERSION 3.14)
+
+set(EBPFPUB_VERSION "1.0.0")
